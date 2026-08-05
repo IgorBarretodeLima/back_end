06cee062687bced022675b43600b8751e779de21
IgorBarretodeLima <igorbarretodelima@gmail.com>
2026-08-05 16:42:58 -0300

    Delete veicolus directory

--- veicolus/src/MainBicicleta.java
+++
@@ -1,7 +0,0 @@
-import static java.lang.IO.*;
-void main(){
-    Bicicleta bicicleta = new Bicicleta("Caloi Vulcan", "29", "Aluminio");
-    IO.println(bicicleta);
-
-
-}
--- veicolus/src/MainMoto.java
+++
@@ -1,7 +0,0 @@
-import static java.lang.IO.*;
-void main(){
-    Moto moto = new Moto("Honda CG", "1 cilindro", "Tipo A");
-    IO.println(moto);
-
-
-}

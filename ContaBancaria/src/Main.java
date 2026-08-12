cb729855f9f8e21faba0985743503596339485da
IgorBarretodeLima <igorbarretodelima@gmail.com>
2026-08-12 15:36:54 -0300

    Delete ContaBancaria directory

--- ContaBancaria/src/Main.java
+++
@@ -1,8 +0,0 @@
-import static java.lang.IO.*;
-void main() {
-    ContaBancaria c4=new ContaBancaria();
-    c4.setTitular("casio");
-    c4.depositar(500);
-    c4.sacar(500);
-    c4.getSaldo();
-}

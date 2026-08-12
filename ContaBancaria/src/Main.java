3173fe0d02d76dd76388d29e541366a4c8fd46ee
IgorBarretodeLima <igorbarretodelima@gmail.com>
2026-08-12 15:37:06 -0300

    Add files via upload

---
+++ ContaBancaria/src/Main.java
@@ -0,0 +1,12 @@
+import static java.lang.IO.*;
+void main() {
+    ContaBancaria c4=new ContaBancaria();
+
+    c4.setTitular("igor");
+
+    c4.depositar(500);
+
+    c4.sacar(500);
+
+    c4.getSaldo();
+}

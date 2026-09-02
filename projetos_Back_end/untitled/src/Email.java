45f1dff2eba982c1c97c3f4774302b6a0afec0f9
IgorBarretodeLima <igorbarretodelima@gmail.com>
2026-09-02 14:37:44 -0300

    Delete projetos_Back_end directory

--- projetos_Back_end/untitled/src/Email.java
+++
@@ -1,8 +0,0 @@
-import static java.lang.IO.*;
-void main(){
-    Usuario usuario = new Usuario("paulo", "1234", "paulo@gmail.com");
-    IO.println(usuario);
-    usuario.email = "paulindodograu244@gmail.com";
-
-
-}
--- projetos_Back_end/untitled/src/Main.java
+++
@@ -1,8 +0,0 @@
-
-import static java.lang.IO.*;
-void main(){
-    Livro livro= new Livro("Churches of Paris", "igor", "casa do igor", 450);
-    IO.println(livro);
-
-
-}
